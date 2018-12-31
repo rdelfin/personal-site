b66318b812a255f0af3e7ecffaf264b06015173b
Ricardo Delfin <me@rdelfin.com>
2018-12-31 01:40:56 +0000

    Moved all ssp templates to jinja for use in flask

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template, send_from_directory
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
@@ -12,6 +12,41 @@ def send_static(path):
 @application.route('/documents/<path:path>')
 def send_document(path):
     return send_from_directory('documents', path)
+
+
+@application.route('/')
+def index():
+    return render_template('home.html')
+ 
+
+@application.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+
+@application.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@application.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+
+@application.route('/projects/car')
+def car_project():
+    return render_template('projects/car.html')
+
+
+@application.route('/projects/twitcher')
+def twitcher_project():
+    return render_template('projects/twitcher.html')
+
+
+@application.route('/blog/post<int:post_num>')
+def blog_post(post_num):
+    return render_template(f'blog/post{post_num}.html')
 
 
 # run the app.
