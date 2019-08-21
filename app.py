a6eb1855fe0dc7bb61c7bf18349a93cb0f83730b
Ricardo Delfin <me@rdelfin.com>
2019-08-21 22:55:51 +0100

    Fixed typo un app.py

--- app.py
+++ app.py
@@ -55,5 +55,5 @@ def blog_post(post_num: int) -> Response:
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    appl.debug = True
-    appl.run()
+    app.debug = True
+    app.run()
