2f2a9cca0cffc398eb3af05f5eb44b3c95322ff5
Ricardo Delfin <me@rdelfin.com>
2021-07-25 22:31:20 +0100

    Added a header and sidebar

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import './index.css';
-import App from './App';
+import "fontsource-roboto";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
