702b2be1d2219316e72b6934d2a63afdd98f2153
Ricardo Delfin <me@rdelfin.com>
2021-10-29 14:58:49 +0100

    Added DB schema

---
+++ api/sql/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Project(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    image_url TEXT NOT NULL,
+    short_desc TEXT NOT NULL,
+    content TEXT NOT NULL
+);
